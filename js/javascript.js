6512cde9330dc3a6d0f0f74db6ac5db1fd21a2f7
Santiago Mascuka <mascuka410@gmail.com>
2023-02-21 15:38:11 -0300

    seguna subida

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+console.log("hola")
+
+
+function cambiarColor(elemento) {
+    if (elemento.style.color === 'black') {
+      elemento.style.color = '#3D5A6C';
+    } else {
+      elemento.style.color = 'black';
+    }
+  }
